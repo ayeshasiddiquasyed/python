765ac17e0b5ca23b8c1f21c8fcc3beff541e65a1
Syed.Ayesha Siddiqua <syedayeshasiddiqua57@gmail.com>
2025-03-12 21:41:04 +0530

    Add files via upload

--- leapyear.py
+++ leapyear.py
@@ -1,7 +1,27 @@
-year=int(input("enter year  :"))
-if(year%4==0):
-  if(year%100!=0):
-    if(year%4==0):
-      print("leap year")
+year=int(input("enter year :"))
+if(year%4==0 and year %100!=0)or(year%400==0):
+    print("leap year")
 else:
-  print("not a leap year")
+    print("not leap year")
+print("----------------")
+x='12344321'
+if x==x[::-1]:
+    print("palindrome")
+else:
+    print("not a palindrome")
+print("----------------")
+x=input("enter alphabet:").lower()
+if x in('a','e','i','o','u'):
+    print("voels")
+else:
+    print("consonant")
+print("--------------")
+x=input("enter color  :").lower()
+if x in ('red'):
+    print('stop')
+elif x in('yellow'):
+    print('slow')
+else:
+    print('Go')
+
+
